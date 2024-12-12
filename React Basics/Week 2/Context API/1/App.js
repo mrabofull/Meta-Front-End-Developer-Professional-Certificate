0e66be836d2e868e1f314d1f797d5e457dc87d80
mrabofull <mrabofull@gmail.com>
2024-12-12 16:46:25 +0200

    Context API

---
+++ React Basics/Week 2/Context API/1/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import MealsProvider from "./providers/MealsProvider";
+import MealsList from "./components/MealsList";
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <MealsProvider>
+        <MealsList />
+        <Counter />
+      </MealsProvider>
+    </div>
+  );
+}
+
+export default App;
