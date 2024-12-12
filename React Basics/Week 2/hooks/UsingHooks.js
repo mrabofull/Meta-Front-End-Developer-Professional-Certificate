dc3208f313cdfdaf564c91db0e030ff78b0dc901
mrabofull <mrabofull@gmail.com>
2024-12-12 09:39:33 +0200

    Using Hooks

---
+++ React Basics/Week 2/hooks/UsingHooks.js
@@ -0,0 +1,98 @@
+import { useState } from 'react';
+
+export default function InputComponent() { 
+  const [inputText, setText] = useState('hello'); 
+
+  function handleChange(e) { 
+    setText(e.target.value); 
+  } 
+
+  return ( 
+    <> 
+      <input value={inputText} onChange={handleChange} /> 
+      <p>You typed: {inputText}</p> 
+      <button onClick={() => setText('hello')}> 
+        Reset 
+      </button> 
+    </> 
+  ); 
+} 
+
+
+// ---------------------------------------------
+
+import { useState } from 'react'; 
+
+export default function RegisterForm() { 
+  const [form, setForm] = useState({ 
+    firstName: 'Luke', 
+    lastName: 'Jones', 
+    email: 'lukeJones@sculpture.com', 
+  }); 
+
+  return ( 
+    <> 
+      <label> 
+        First name: 
+        <input 
+          value={form.firstName} 
+          onChange={e => { 
+            setForm({ 
+              ...form, 
+              firstName: e.target.value 
+            }); 
+          }} 
+        /> 
+      </label> 
+      <label> 
+        Last name: 
+        <input 
+          value={form.lastName} 
+          onChange={e => { 
+            setForm({ 
+              ...form, 
+              lastName: e.target.value 
+            }); 
+          }} 
+        /> 
+      </label> 
+      <label> 
+        Email: 
+        <input 
+          value={form.email} 
+          onChange={e => { 
+            setForm({ 
+              ...form, 
+              email: e.target.value 
+            }); 
+          }} 
+        /> 
+      </label> 
+      <p> 
+        {form.firstName}{' '} 
+        {form.lastName}{' '} 
+        ({form.email})
+      </p> 
+    </> 
+  ); 
+} 
+
+
+// --------------------------------------
+
+// The useRef hook
+// useRed is used to access a child elemnt directly.
+
+function TextInputWithFocusButton() {
+  const inputEl = useRef(null);
+  const onButtonClick = () => {
+    // `current` points to the mounted text input element
+    inputEl.current.focus();
+  };
+  return (
+    <>
+      <input ref={inputEl} type="text" />
+      <button onClick={onButtonClick}>Focus the input</button>
+    </>
+  );
+}
